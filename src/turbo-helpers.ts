b63187fe4056bcf5285904461e58ccdc68a38a62
Alexandre Boucher <alexandre@trampoline.cx>
2024-06-05 15:59:44 -0400

    feat: add support for turbo v2

---
+++ src/turbo-helpers.ts
@@ -0,0 +1,96 @@
+import { debug, getInput, setFailed } from '@actions/core'
+import { execSync } from 'child_process'
+import { join } from 'path'
+import semver from 'semver'
+
+export type TurboVersion = 1 | 2
+
+/**
+ * Get the major version of Turborepo used.
+ * 1. Prioritize looking at the input passed to the action.
+ * 2. If not provided, looks at the package.json of workingDirectory to infer the turbo version from.
+ *
+ * @returns 1 | 2
+ */
+export const getTurboMajorVersion = (workingDirectory: string): TurboVersion | false => {
+  let turboMajorVersion = getInput('turbo-major-version', { required: false })
+
+  if (!turboMajorVersion) {
+    debug('Reading package.json to find turbo version')
+    const { devDependencies, dependencies } = require(join(workingDirectory, 'package.json'))
+
+    const turboMajorVersion = devDependencies?.turbo || dependencies?.turbo
+
+    if (!turboMajorVersion) {
+      setFailed(
+        `Couldn't find turbo dependency in root package.json. Please provide turbo-major-version as an input to the Github Action.`,
+      )
+      return false
+    }
+  }
+
+  const major = semver.major(turboMajorVersion)
+
+  if (major < 1 || major > 2) {
+    setFailed(`Expected turbo major version to be either 1 or 2, detected version ${major}.`)
+    return false
+  } else {
+    return major as TurboVersion
+  }
+}
+
+export interface GetTurboChangedWorkspacesOptions {
+  majorTurboVersion: TurboVersion
+  turboTaskName: string
+  workspace: string
+  from: string
+  to: string
+  workingDirectory: string
+}
+
+export interface ChangedWorkspaces {
+  changed: boolean
+  affectedWorkspaces: string[]
+}
+
+export const getTurboChangedWorkspaces = async ({
+  majorTurboVersion,
+  turboTaskName,
+  workspace,
+  from,
+  to,
+  workingDirectory,
+}: GetTurboChangedWorkspacesOptions): Promise<ChangedWorkspaces> => {
+  // if (majorTurboVersion === 1) {
+  const json = await execSync(
+    `TURBO_TELEMETRY_DISABLED=1 TURBO_TELEMETRY_MESSAGE_DISABLED=1 npx turbo@^${majorTurboVersion} run ${turboTaskName} --filter="${workspace}...[${from}...${to}]" --dry-run=json`,
+    {
+      cwd: join(process.cwd(), workingDirectory),
+      encoding: 'utf-8',
+    },
+  )
+
+  debug(`Output from Turborepo: ${json}`)
+
+  const parsedOutput = JSON.parse(json)
+  const changed = parsedOutput.packages.includes(workspace)
+
+  return { changed, affectedWorkspaces: parsedOutput.packages }
+  // } else {
+  //   // Turbo v2
+  //   const json = await execSync(
+  //     `TURBO_TELEMETRY_DISABLED=1 TURBO_TELEMETRY_MESSAGE_DISABLED=1 npx turbo@^1 run ${turboTaskName} --filter="${workspace}...[${from}...${to}]" --dry-run=json`,
+  //     {
+  //       cwd: join(process.cwd(), workingDirectory),
+  //       encoding: 'utf-8',
+  //     },
+  //   )
+
+  //   debug(`Output from Turborepo: ${json}`)
+
+  //   const parsedOutput = JSON.parse(json)
+  //   const changed = parsedOutput.packages.includes(workspace)
+
+  //   return { changed, affectedWorkspaces: parsedOutput.packages }
+  // }
+}
